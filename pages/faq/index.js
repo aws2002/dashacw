b3150b198f512d5a66b32b8bff6700e15eb6645c
Osama Husam <osamahusam848@gmail.com>
2022-09-06 08:57:17 +0300

    edit serve-json

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -36,7 +36,7 @@ export default function Faq({fags}) {
     </Layout>
   );
 }
-export async function getStaticProps() {
+export async function getServerSideProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
   const res = await fetch('http://localhost:4000/faqs')
